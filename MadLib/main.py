1e94a487c36673a0002ef4fc65637a1a70c99bf0
Jessica Hernandez <jesherart@fullsail.edu>
2015-08-05 23:51:30 -0400

    Commit 3:
    Initializing more variables are the inputs from the user using
    raw_input. Met the input/output, datatype, 3 strings and 3 numbers
    requirements.
    
    Used an array to store number inputs; met the array requirement.
    
    Created a dictionary and used a conditional to set the value of a
    variable using the dictionary depending on what the choice of the user
    was; met the dictionary requirement.

--- MadLib/main.py
+++ MadLib/main.py
@@ -20,8 +20,30 @@ welcome_message = '''
 print welcome_message
 
 raw_input("Your questions will now begin. Press enter to continue.\n\n")
+
 adjective = raw_input("  1. Enter an adjective:  ")
-exclamations = raw_input("  2. Enter your favorite number:  ")
+exclamations = int(raw_input("  2. Enter your favorite number:  "))
 feeling1 = raw_input("  3. Enter a feeling:  ")
 cartoon_character = raw_input("  4. Enter the name of a cartoon character:  ")
 candy = raw_input("  5. Enter the name of your favorite candy:  ")
+feeling2 = raw_input("  6. Enter another feeling:  ")
+
+print "  7. You will now enter four different numbers as prompted"
+winning_numbers = []
+winning_numbers.append(int(raw_input("\tEnter a number between 1-50:  ")))
+winning_numbers.append(int(raw_input("\tEnter a second number between 1-50:  ")))
+winning_numbers.append(int(raw_input("\tEnter a third number between 1-50:  ")))
+winning_numbers.append(int(raw_input("\tEnter a fourth number between 1-50:  ")))
+
+dog_food = raw_input("  8. Enter the name of a brand of dog food:  ")
+
+colors = dict()
+colors = {"red":10,"blue":20,"green":30}
+color_choice = raw_input("  9. Which color do you prefer? red, blue or green?:  ")
+
+if color_choice == "red":
+    money_amount = colors["red"]
+elif color_choice == "blue":
+    money_amount = colors["blue"]
+elif color_choice == "green":
+    money_amount = colors["green"]
