31d114f5f3416aa1ca2f58a75a0f522811906643
Jessica Hernandez <jesherart@fullsail.edu>
2015-08-06 21:58:24 -0400

    Commit 5:
    Put together the final message that’s output to the user once the
    questions have been answered and the madlib story gets put together.

--- MadLib/main.py
+++ MadLib/main.py
@@ -51,24 +51,77 @@ elif color_choice == "green":
 car = raw_input(" 10. Enter the Make and Model of your favorite car:  ")
 cereal = raw_input(" 11. Enter the name of any cereal:  ")
 element = raw_input(" 12. Enter the name of any element on the Periodic Table:  ")
-unusual_job = raw_input(" 13. Enter the name of an unusual job:  ")
+unusual_job = raw_input(" 13. Enter the name of an uncommon job:  ")
 
 addition = 0
+string_of_numbers = ""
 for i in range(0,len(winning_numbers)):
     addition += winning_numbers[i]
+    
+    if i == len(winning_numbers) - 1:
+        string_of_numbers += "and " + str(winning_numbers[i])
+    else:
+        string_of_numbers += str(winning_numbers[i]) + ", "
+    
     i += 1    
 
 if addition <= 66:
-    money_amount -= favorite_number
+    money_amount += favorite_number
 elif addition >= 67 & addition <= 132:
     money_amount *= favorite_number
 elif addition >= 133 & addition <= 200:
-    money_amount += favorite_number
+    money_amount = addition / favorite_number
 
 def jobCreator(adj,job):    
-    job_creation = adj + " " + job
-    return job_creation
+    new_job = adj + " " + job
+    return new_job
 
-new_job = jobCreator(adjective,unusual_job);
+wacky_job = jobCreator(adjective,unusual_job);
+
+exclamations = ""
+for i in range(0,favorite_number):
+    exclamations += "!"
+    i += 1
 
 raw_input("\n\nYour new diary entry was submitted. Press enter to continue.\n\n")
+
+import datetime
+d = datetime.datetime.now()
+print ("\t|\n\t|\tDATE: %s/%s/%s" % (d.month, d.day, d.year))
+
+diary_entry = '''\t|
+\t|\tMy dearest {color_choice} diary, 
+\t|
+\t|\tToday has been the most peculiar day I've had in a long time...
+\t|
+\t|\tI won the lotto{exclamations}
+\t|
+\t|\tIt all started when I was feelling {feeling1} and I decided to go
+\t|\tto the {cartoon_character} gas station to fill up my tank. As I
+\t|\twas about to grab some {candy}, I thought about using the money
+\t|\tto buy a lotto ticket instead because I was having this {feeling2}
+\t|\tfeeling that I should play the numbers: {string_of_numbers}.
+\t|\tAs I was waiting for it to be 6PM, I had a strange urge to eat
+\t|\ta bowl of {dog_food} dog food. Then the time had come and the
+\t|\twinning numbers were displayed.. and it was I who had won
+\t|\tthe {money_amount} million dollars!!! First thing that came to mind
+\t|\twas that I had to go buy my dream car, a {car}. I immediately
+\t|\traced down to the nearest dealer to go buy my new car. After, as I'm
+\t|\tdriving down the {cereal} express way to break in my new car, I
+\t|\thear a sudden {element} explosion between the two vehicles driving right
+\t|\tbehind me and that's when... I woke up and realized that I in fact
+\t|\tdid NOT win the lotto and I most definitely was not able to buy my
+\t|\tbeloved {car}. Talk about a bubble buster! Therefore, I need to
+\t|\tcontinue living my life as a {wacky_job}, which will
+\t|\tprobably never earn me enough anyways. So please {color_choice} diary...
+\t|\tnever speak of this "day" to me ever again.
+\t|
+\t|\tStay awesome Gotham!
+\t|
+
+'''
+
+diary_entry = diary_entry.format(**locals())
+print diary_entry
+
+raw_input("Thank you for playing. Press enter to exit.\n\n")
