f9bf713790214434e58e819f792ea4f6df749d83
Jessica Hernandez <jesherart@fullsail.edu>
2015-08-06 21:10:22 -0400

    Commit 4:
    Added in the last variables.
    Created the for loop to get the addition of all the numbers in the
    winning_numbers array.
    Created conditional to come up with the final money amount to be won in
    the lotto.
    Created a function to concatenate the adjective with the unusual job to
    create a wacky job.

--- MadLib/main.py
+++ MadLib/main.py
@@ -22,7 +22,7 @@ print welcome_message
 raw_input("Your questions will now begin. Press enter to continue.\n\n")
 
 adjective = raw_input("  1. Enter an adjective:  ")
-exclamations = int(raw_input("  2. Enter your favorite number:  "))
+favorite_number = int(raw_input("  2. Enter your favorite number:  "))
 feeling1 = raw_input("  3. Enter a feeling:  ")
 cartoon_character = raw_input("  4. Enter the name of a cartoon character:  ")
 candy = raw_input("  5. Enter the name of your favorite candy:  ")
@@ -39,7 +39,7 @@ dog_food = raw_input("  8. Enter the name of a brand of dog food:  ")
 
 colors = dict()
 colors = {"red":10,"blue":20,"green":30}
-color_choice = raw_input("  9. Which color do you prefer? red, blue or green?:  ")
+color_choice = raw_input("  9. Enter the color that you prefer (red, blue or green):  ")
 
 if color_choice == "red":
     money_amount = colors["red"]
@@ -47,3 +47,28 @@ elif color_choice == "blue":
     money_amount = colors["blue"]
 elif color_choice == "green":
     money_amount = colors["green"]
+
+car = raw_input(" 10. Enter the Make and Model of your favorite car:  ")
+cereal = raw_input(" 11. Enter the name of any cereal:  ")
+element = raw_input(" 12. Enter the name of any element on the Periodic Table:  ")
+unusual_job = raw_input(" 13. Enter the name of an unusual job:  ")
+
+addition = 0
+for i in range(0,len(winning_numbers)):
+    addition += winning_numbers[i]
+    i += 1    
+
+if addition <= 66:
+    money_amount -= favorite_number
+elif addition >= 67 & addition <= 132:
+    money_amount *= favorite_number
+elif addition >= 133 & addition <= 200:
+    money_amount += favorite_number
+
+def jobCreator(adj,job):    
+    job_creation = adj + " " + job
+    return job_creation
+
+new_job = jobCreator(adjective,unusual_job);
+
+raw_input("\n\nYour new diary entry was submitted. Press enter to continue.\n\n")
