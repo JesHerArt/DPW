1f4c760e975846df752173529cf25d877007c70e
Jessica Hernandez <jesherart@fullsail.edu>
2015-08-13 21:27:26 -0400

    Commit 3: Simple Form Assignment
    Modified app.yam file to include images directory.
    Modified style.css for style and layout testing of the form page.
    Adding baby_turtle.jpg to images directory.
    Modified pages.py file in the self.header and self.body sections for
    html code of the form and header image/text.

--- simple-form/pages.py
+++ simple-form/pages.py
@@ -10,6 +10,7 @@ class Page(object):
     def __init__(self):
         self.title = "Sea Turtle Donation Form"
         self.css = "css/style.css"
+        self.header_img = "images/baby_turtle.jpg"
         self.head = '''
 <!doctype html>
 <html>
@@ -21,8 +22,87 @@ class Page(object):
         <div id="container">
         '''
         
-        self.body = '''
+        self.header = '''
+            <header>
+                <img src="{self.header_img}" alt="Turtle hatching on its way to the sea" title="Turtle hatching on its way to the sea" />
+                <h1>Sea Turtle Donation Form</h1>
+                <h2>Help protect our loving sea turtles from becoming endangered any further. Every donation helps contibute to the rehabilitation costs for Sea Turtles all around the world.</h2>
+            </header>
+        '''
         
+        self.body = '''
+            <form method="GET" id="donation_form">
+                <div class="form-group">
+                    <div class="left-col">
+                        <label for="full_name">Name</label>
+                    </div>
+                    <div class="right-col">
+                        <input type="text" id="full_name" name="full_name" placeholder="Full Name" required />
+                    </div>
+                </div>
+                
+                <div class="form-group">
+                    <div class="left-col">
+                        <label for="email">Email</label>
+                    </div>
+                    <div class="right-col">
+                        <input type="email" id="email" name="email" placeholder="Email Address" required />
+                    </div>
+                </div>
+                
+                <div class="form-group">
+                    <div class="left-col">
+                        <label for="phone">Phone</label>
+                    </div>
+                    <div class="right-col">
+                        <input type="text" id="phone" name="phone" placeholder="(000) 000-0000" required />
+                    </div>
+                </div>
+                
+                <div class="form-group">
+                    <div class="left-col">
+                        <label for="amount">Donation Amount</label>
+                    </div>
+                    <div class="right-col">
+                        <div class="billing_option"><input type="radio" name="amount" value="5.00" /> <p class="radio-option">$5.00</p></div>
+                        <div class="billing_option"><input type="radio" name="amount" value="10.00" /> <p class="radio-option">$10.00</p></div>
+                        <div class="billing_option"><input type="radio" name="amount" value="20.00" /> <p class="radio-option">$20.00</p></div>
+                        <div class="billing_option"><input type="radio" name="amount" value="30.00" /> <p class="radio-option">$30.00</p></div>
+                        <div class="billing_option"><input type="radio" name="amount" value="50.00" /> <p class="radio-option">$50.00</p></div>
+                    </div>
+                </div>
+                
+                <div class="form-group">
+                    <div class="left-col">
+                        <label for="billing">Billing Frequency</label>
+                    </div>
+                    <div class="right-col">
+                        <select>
+                            <option id="" value=""></option>
+                        </select>
+                    </div>
+                </div>
+                
+                <div class="form-group">
+                    <div class="left-col">
+                        <label for="payment">Payment Option</label>
+                    </div>
+                    <div class="right-col">
+                        <div class="payment_option"><input type="radio" name="payment" value="Cash" /> <p class="radio-option">Cash (Send my invoice by mail)</p></div>
+                        <hr>
+                        <div class="payment_option"><input type="radio" name="payment" value="Credit Card" /> <p class="radio-option">Credit Card</p></div>
+                        <div class="payment_option"><input type="radio" name="payment" value="PayPal" /> <p class="radio-option">PayPal</p></div>
+                        <div class="payment_option"><input type="radio" name="payment" value="Google Wallet" /> <p class="radio-option">Google Wallet</p></div>
+                    </div>
+                </div>
+                
+                <div class="form-group">
+                    <div class="left-col"></div>
+                    <div class="right-col">
+                        <input type="submit" value="Submit" />
+                    </div>
+                </div>
+            </form>
         '''
         
         self.close = '''
@@ -32,6 +112,6 @@ class Page(object):
         '''
         
     def print_out(self):
-        page_construct = self.head + self.body + self.close
+        page_construct = self.head + self.header + self.body + self.close
         page_construct = page_construct.format(**locals())
         return page_construct
