3295a11c823f30ce54eeb24497a69872cea9d3b2
Jessica Hernandez <jesherart@fullsail.edu>
2015-08-19 17:22:19 -0400

    Commit 2: Reusable Library Assignment
    Began creating the attributes and getters for pages.py.

--- reusable-library/pages.py
+++ reusable-library/pages.py
@@ -8,7 +8,32 @@ Simple Form Assignment
 
 class FormPage(object):
     def __init__(self):
-        pass
+        self.__title = "Miami-Dade County Animal Shelter | Dog Adoption Suggestion Form"
+        self.__head = """
+<!doctype html>
+<html>
+    <head>
+        <title>{self.title}</title>
+        <link type="text/css" rel="stylesheet" href="css/style.css"/>
+        <!--Google Fonts-->
+    </head>
+    <body>
+        <div id="container">
+        """
+        
+        self.__body = """
+        
+        """
+        
+        self.__close = """
+        </div>
+    </body>
+</html>
+        """
+    
+    @property
+    def title(self):
+        return self.__title
 
 class ResultPage(object):
     def __init__(self):
