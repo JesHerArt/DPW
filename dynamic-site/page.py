9db408bdebd7b74bf727e2b26b77277d1a3740ca
Jessica Hernandez <jesherart@fullsail.edu>
2015-08-27 19:32:36 -0400

    Commit 2: Dynamic Site Assignment
    Beginning to build up the Page class.

--- dynamic-site/page.py
+++ dynamic-site/page.py
@@ -7,6 +7,42 @@ Dynamic Site Assignment
 '''
 
 #ABSTRACT PAGE CLASS
+class Page(object):
+    def __init__(self):
+        self.__title = "Skylanders Spyro's Adventure Characters"
+        
+        self.head = '''
+<!doctype html>
+<html>
+    <head>
+        <title>{self.title}</title>
+        <link type="text/css" rel="stylesheet" href="css/style.css"/>
+        <!--Google Fonts-->
+        
+    </head>
+    <body>
+        <div id="container">
+        '''
+        
+        self.body = '''
+        
+        '''
+        
+        self.content = '''
+        
+        '''
+        
+        self.close = '''
+        </div>
+        <footer>
+            <p>&copy; 2015 Jessica J. Hernandez - Student at Full Sail University<br>All content taken from: <a href="https://www.skylanders.com/characters#fire/ssa" target="_blank">https://www.skylanders.com/characters#fire/ssa</a></p>
+        </footer>
+    </body>
+</html>
+        '''
 
 
 #CONTENT PAGE CLASS
+class ContentPage(Page):
+    def __init__(self):
+        
