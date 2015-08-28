2ce9b57ec3c546cffcb5b40cdd4221ca35d2155e
Jessica Hernandez <jesherart@fullsail.edu>
2015-08-27 21:22:49 -0400

    Commit 7: Dynamic Site Assignment
    Setting up the conditional statements in the main.py depending on the
    get variable that was passed in.

--- dynamic-site/main.py
+++ dynamic-site/main.py
@@ -7,12 +7,41 @@ Dynamic Site Assignment
 '''
 
 import webapp2
+from pages import Page, ContentPage
+from data import Skylander, Data
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         
         #conditional to verify if any GET key/value pairs exists
         if self.request.GET:
+            skylander = self.request.GET["skylander"]
+            p = ContentPage()
+            
+            if(skylander == "eruptor"):
+                s = Data()
+                s.skylanders[0]
+            elif(skylander == "gill"):
+                s = Data()
+                s.skylanders[1]
+            elif(skylander == "terra"):
+                s = Data()
+                s.skylanders[2]
+            elif(skylander == "elf"):
+                s = Data()
+                s.skylanders[3]
+            elif(skylander == "warnado"):
+                s = Data()
+                s.skylanders[4]
+            elif(skylander == "trigger"):
+                s = Data()
+                s.skylanders[5]
+            elif(skylander == "spyro"):
+                s = Data()
+                s.skylanders[6]
+            elif(skylander == "hex"):
+                s = Data()
+                s.skylanders[7]
             
             self.response.write('print out dynamic page based on get value')
         else:
